95ae85f9d0c4ba388a5451a5833727ed691de981
ManoelFer <manoelfernandes15@gmail.com>
2022-06-26 15:39:00 -0300

    🐛 resolve bug, ormconfig.ts module not found

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,20 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
 
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "graphql_database",
+      entities: [
+        "dist/**/*.entity{.ts,.js}"
+      ],
+      synchronize: true
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
