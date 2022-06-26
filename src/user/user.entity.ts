4845d91540ceee50283410a8f4751f12f16bd322
ManoelFer <manoelfernandes15@gmail.com>
2022-06-26 13:54:34 -0300

    ✨ modify user service

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -5,7 +5,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType() //Informa que é um objeto
 @Entity() //Especifica ao código que essa classe se trata de uma entidade
-export class Author {
+export class User {
     @PrimaryGeneratedColumn() // Especifica que a coluna id é uma coluna de chave primaria que é gerada automaticamente
     @Field(() => ID) // Informa ao graphql o tipo da coluna
     id: string; // tipagem da coluna
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+// importações
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity'
 
-@Injectable()
-export class UserService {}
+@Injectable() // para fazer injeção de dependências no nestjs
+export class UserService {
+
+    //injeta o repositório utilizando a entidade de usuário
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) { }
+
+    //método para criar um usuário
+    async createUser(data: CreateUserInput): Promise<User> {
+        const user = await this.userRepository.create(data);
+        const userSaved = await this.userRepository.save(user)
+
+        if (!userSaved) {
+            throw new InternalServerErrorException('Falha na criação do usuário')
+        }
+
+        return userSaved
+    }
+}
