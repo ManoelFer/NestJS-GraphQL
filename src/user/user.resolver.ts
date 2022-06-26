64f9b9a459039d02c68c5dcafbfede374bc8aba6
ManoelFer <manoelfernandes15@gmail.com>
2022-06-26 14:01:04 -0300

    ✨ change user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+// importações
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver('User')
+export class UserResolver {
+    //Ejetar nosso userService
+    constructor(private userService: UserService) { }
+
+    @Mutation()//informa ao graphql que esse método vai modificar o estado no nosso banco de dados
+    async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+        const user = await this.userService.createUser(data)
+        return user
+    }
+}
